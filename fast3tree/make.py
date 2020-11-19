b9c715520bc5d5bad93d387cbec3f436b6d37b3b
Yao-Yuan Mao <yymao.astro@gmail.com>
2020-11-18 21:51:27 -0500

    Allow input src_dir and supress raise

--- fast3tree/make.py
+++ fast3tree/make.py
@@ -1,21 +1,21 @@
 import os
 import tempfile
-from subprocess import check_call
+from subprocess import check_call, CalledProcessError
 
 __all__ = ["make_lib"]
 
 _TMPDIR = None
 
 
-def make_lib(dim, use_double, remake=False, path=None):
+def make_lib(dim, use_double, remake=False, src_dir=None, lib_dir=None, raise_if_fall=True):
     global _TMPDIR
 
     dim = int(dim)
     if dim < 2:
         raise ValueError("`dim` needs to be at least 2.")
 
-    src_dir = os.path.abspath(os.path.dirname(__file__))
-    lib_dir = path or src_dir
+    src_dir = src_dir or os.path.abspath(os.path.dirname(__file__))
+    lib_dir = lib_dir or src_dir
 
     lib_name = "fast3tree_{0}_{1}".format(dim, "d" if use_double else "f")
     lib_so_path = os.path.join(lib_dir, lib_name + ".so")
@@ -50,11 +50,16 @@ struct mytype{{int64_t idx; FAST3TREE_FLOATTYPE pos[{0}];}};
                 dim, "double" if use_double else "float", src_dir
             )
         )
-    check_call(
-        "gcc -m64 -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -D_DEFAULT_SOURCE -D_POSIX_SOURCE -D_POSIX_C_SOURCE=200809L -D_SVID_SOURCE -D_DARWIN_C_SOURCE -Wall -fno-math-errno -fPIC -shared {0} -o {1} -lm -O3 -std=c99".format(
-            lib_c_path, lib_so_path
-        ).split()
-    )
-    os.remove(lib_c_path)
+
+    call_args = "gcc -m64 -D_LARGEFILE_SOURCE -D_LARGEFILE64_SOURCE -D_FILE_OFFSET_BITS=64 -D_DEFAULT_SOURCE -D_POSIX_SOURCE -D_POSIX_C_SOURCE=200809L -D_SVID_SOURCE -D_DARWIN_C_SOURCE -Wall -fno-math-errno -fPIC -shared {0} -o {1} -lm -O3 -std=c99".format(
+        lib_c_path, lib_so_path
+    ).split()
+    try:
+        check_call(call_args)
+    except CalledProcessError:
+        if raise_if_fall:
+            raise
+    finally:
+        os.remove(lib_c_path)
 
     return lib_so_path
